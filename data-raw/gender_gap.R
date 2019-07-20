9c031d767b39d33ef3de9667bdc2d59290ee87c7
Michael Clark <micl@umich.edu>
2019-07-20 16:52:48 -0400

    gender gap complete
    Former-commit-id: 2a7d4f22121d69a1242449c3184d19a75601a909 [formerly 205b766ba7f373f96a0322bd8d9e65afd1843cf8]
    Former-commit-id: 2a7d4f22121d69a1242449c3184d19a75601a909
    Former-commit-id: 535a48d5442bbef64fa943ea194d3c3b0f255c6d

--- data-raw/gender_gap.R
+++ data-raw/gender_gap.R
@@ -11,24 +11,30 @@ gender_gap0 %>% describe_all()
 
 
 gender_gap = gender_gap0 %>%
-  filter(subindicator_type != 'Normalized Score') %>%
+  select(-indicator_id) %>%
+  filter(!grepl(subindicator_type, pattern = 'Normalized Score|Rank')) %>%
   gather(key = year, value = value,
-         -country_iso3, -country_name, -indicator_id, -indicator, -subindicator_type) %>%
+         -country_iso3, -country_name, -indicator, -subindicator_type) %>%
   mutate(year = as.integer(str_remove(year, pattern = 'x'))) %>%
   arrange(country_name, indicator, year, subindicator_type)
 
 gender_gap
 
-# how do you have more ranks than values? go world bank!
-gender_gap %>%
-  spread(key = subindicator_type, value = value)
+gender_gap_2018 = gender_gap %>%
+  filter(year == 2018) %>%
+  spread(indicator, value) %>%
+  janitor::clean_names() %>%
+  select(-subindicator_type)
 
-gender_gap %>% tidyext::describe_all_num()
+gender_gap_2018
 
 gender_gap = gender_gap %>%
-  spread(key = `Subindicator Type`, value = value)
+  filter(grepl(indicator, pattern = 'Gender')) %>%
+  spread(key = indicator, value = value) %>%
+  janitor::clean_names() %>%
+  select(-subindicator_type)
 
-# looks like gender gap
-gender_gap %>%
-  spread(key = Indicator, value = Index) %>%
-  tidyext::describe_all_num()
+
+
+usethis::use_data(gender_gap, overwrite = T)
+usethis::use_data(gender_gap_2018, overwrite = T)
