bcdb56d5b75d7ba14fcef6920f020e14eb34a2a0
Michael Clark <micl@umich.edu>
2019-08-25 11:17:55 -0400

    update readme and package description
    Former-commit-id: 2e6857ed261ee9aea0b388f69d4e482f4bc9642f [formerly 362051e0dc2c811d6e1afd18c289b20bfe5dfcf0]
    Former-commit-id: 2e6857ed261ee9aea0b388f69d4e482f4bc9642f
    Former-commit-id: 1a6f5928021ba7c0c9aa77db56daf3f1fcab565b

--- R/noiris-package.R
+++ R/noiris-package.R
@@ -1,16 +1,30 @@
 #' @name noiris
 
-#' @description  This package is primarily to provide data that is more relevant to
-#'  actual data people would typically come across in the wild, or is simply
-#'  more interesting (at least to me).  Far too often examples use iris, mtcars,
-#'  etc. for convenience, but actually are inconvenient for demonstrating real
-#'  data and modeling problems, or are too small to even be realistic.  This
-#'  package will provide larger and at some point messier data.
-#'  @details Right now it has:
+#' @description  The goal of this package is primarily to provide data that is
+#'   more relevant to the kind people would more typically come across in the wild,
+#'   or is simply more interesting (at least to me).  Far too often examples use
+#'   iris, mtcars, etc. for convenience, but these actually are inconvenient for
+#'   demonstrating real data and modeling problems, or are too small to be very
+#'   realistic examples of everyday data.  This package will provide larger and
+#'   at some point messier data, that is better named, better documented, and
+#'   would be useful across a variety of modeling contexts.
+#' @details Right now it has:
 #'
 #' \describe{
-#'   \item{\code{gapminder}}{a 2019 pull from \url{http://www.gapminder.org/data/}}
-#'   \item{\code{starwars}}{a variety of cleaned data sets produced using the \link[rwars]{rwars} package.}
+#'   \item{gapminder_2019}{a 2019 pull from \url{http://www.gapminder.org/data/}}
+#'   \item{starwars}{a variety of cleaned data sets produced using the \link[rwars]{rwars} package.}
+#'   \item{instructor_evaluations}{a nice-sized data set for mixed/multi-level modeling taken from the `lme4` package.}
+#'   \item{pisa}{OECD's Programme for International Student Assessment with international scores for math, science, and reading, covering years 2000-2015.}
+#'   \item{world_happiness}Multiyear data set with country level scores of 'happiness'. From 2019 World Happiness Report, and includes data from 2005-2018.}
+#'   \item{wine_reviews}{Two data sets regarding wine reviews that can be used for a wide range of standard statistical and machine learning.}
+#'   \item{google_apps}{Ratings and other information for Google Play Store apps.}
+#'   \item{fashion_train}{The 'Fashion MNIST'. Image data for clothing items. Also \code{fasion_test}.}
+#'   \item{gender_gap}{Country level data regarding the World Bank Gender Gap Index.}
+#'   \item{kiva}{Lending information from kiva.org online crowdfunding platform.}
+#'   \item{water_risk}{Country and province level data regarding water risk.}
+#'   \item{big_five}{Big Five personality traits.}
+#'   \item{heart_disease}{The UCI heart disease data.}
+#'   \item{}{}
 #'   }
 #'
 #' @keywords internal
